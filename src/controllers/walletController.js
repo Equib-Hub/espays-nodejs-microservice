704f0cf59704fb6eaa37981d47070ab9afa978e5
unknown <kehindedipe@gmail.com>
2025-10-31 15:10:46 +0100

    implemented encryption and description of sensitive data

--- src/controllers/walletController.js
+++ src/controllers/walletController.js
@@ -1,18 +1,23 @@
-const bip39 = require('bip39');
-const { ethers } = require('ethers');
-const ecc = require('tiny-secp256k1');
-const bip32 = require('bip32');
-const bitcoin = require('bitcoinjs-lib');
-const solanaWeb3 = require('@solana/web3.js');
-const { TonClient, WalletContractV4, internal } = require('@ton/ton');
-const { mnemonicNew, mnemonicToPrivateKey } = require('@ton/crypto');
-const { Secp256k1HdWallet } = require('@cosmjs/amino');
-const { Slip10RawIndex } = require('@cosmjs/crypto');
-const { Keyring } = require('@polkadot/keyring');
-const { InMemorySigner } = require('@taquito/signer');
-const algosdk = require('algosdk');
-const StellarHDWallet = require('stellar-hd-wallet');
-const { mnemonicGenerate, mnemonicToMiniSecret, sr25519PairFromSeed } = require('@polkadot/util-crypto');
+const bip39 = require("bip39");
+const { ethers } = require("ethers");
+const ecc = require("tiny-secp256k1");
+const bip32 = require("bip32");
+const bitcoin = require("bitcoinjs-lib");
+const solanaWeb3 = require("@solana/web3.js");
+const { TonClient, WalletContractV4, internal } = require("@ton/ton");
+const { mnemonicNew, mnemonicToPrivateKey } = require("@ton/crypto");
+const { Secp256k1HdWallet } = require("@cosmjs/amino");
+const { Slip10RawIndex } = require("@cosmjs/crypto");
+const { Keyring } = require("@polkadot/keyring");
+const { InMemorySigner } = require("@taquito/signer");
+const algosdk = require("algosdk");
+const StellarHDWallet = require("stellar-hd-wallet");
+const {
+  mnemonicGenerate,
+  mnemonicToMiniSecret,
+  sr25519PairFromSeed,
+} = require("@polkadot/util-crypto");
+const { encryptString } = require("../utils/helpers");
 
 class WalletController {
   async createWallet(req, res) {
@@ -24,17 +29,22 @@ class WalletController {
       // TON Wallet
       const keyPair = await mnemonicToPrivateKey(mnemonicTon);
       const workchain = 0;
-      const wallet = WalletContractV4.create({ workchain, publicKey: keyPair.publicKey });
+      const wallet = WalletContractV4.create({
+        workchain,
+        publicKey: keyPair.publicKey,
+      });
       const client = new TonClient({
-        endpoint: 'https://toncenter.com/api/v2/jsonRPC',
+        endpoint: "https://toncenter.com/api/v2/jsonRPC",
       });
       const contract = client.open(wallet);
-      const tonPublicKey = keyPair.secretKey.toString('hex');
-      const tonPrivateKey = keyPair.publicKey.toString('hex');
+      const tonPublicKey = keyPair.secretKey.toString("hex");
+      const tonPrivateKey = keyPair.publicKey.toString("hex");
       const tonAddress = contract.address.toString();
 
       // Ethereum Wallet
-      const hdNode = ethers.HDNodeWallet.fromMnemonic(ethers.Mnemonic.fromPhrase(mnemonic));
+      const hdNode = ethers.HDNodeWallet.fromMnemonic(
+        ethers.Mnemonic.fromPhrase(mnemonic)
+      );
       const ethWallet = new ethers.Wallet(hdNode.privateKey);
       const ethPrivateKey = ethWallet.privateKey;
       const ethAddress = ethWallet.address;
@@ -51,7 +61,7 @@ class WalletController {
       // Solana Wallet
       const solSeed = seed.slice(0, 32);
       const solKeypair = solanaWeb3.Keypair.fromSeed(solSeed);
-      const solPrivateKey = Buffer.from(solKeypair.secretKey).toString('hex');
+      const solPrivateKey = Buffer.from(solKeypair.secretKey).toString("hex");
       const solAddress = solKeypair.publicKey.toBase58();
 
       // Cosmos Wallet
@@ -63,23 +73,26 @@ class WalletController {
         Slip10RawIndex.normal(0),
       ];
       const cosmosWallet = await Secp256k1HdWallet.fromMnemonic(mnemonic, {
-        prefix: 'cosmos',
+        prefix: "cosmos",
         hdPaths: [cosmosHdPath],
       });
-      const [{ address: cosmosAddress, privkey: cosmosPrivateKeyRaw }] = await cosmosWallet.getAccountsWithPrivkeys();
-      const cosmosPrivateKey = Buffer.from(cosmosPrivateKeyRaw).toString('hex');
+      const [{ address: cosmosAddress, privkey: cosmosPrivateKeyRaw }] =
+        await cosmosWallet.getAccountsWithPrivkeys();
+      const cosmosPrivateKey = Buffer.from(cosmosPrivateKeyRaw).toString("hex");
 
       // Polkadot Wallet
-      const keyring = new Keyring({ type: 'sr25519', ss58Format: 0 });
+      const keyring = new Keyring({ type: "sr25519", ss58Format: 0 });
       const polkadotSeed = mnemonicToMiniSecret(mnemonic);
       const { publicKey, secretKey } = sr25519PairFromSeed(polkadotSeed);
       const polkadotAddress = keyring.encodeAddress(publicKey);
-      const polkadotPrivateKey = Buffer.from(secretKey).toString('hex');
+      const polkadotPrivateKey = Buffer.from(secretKey).toString("hex");
 
       // Algorand Wallet
       const algorandAccount = algosdk.generateAccount();
       const algorandAddress = algorandAccount.addr.toString();
-      const algorandPrivateKey = Buffer.from(algorandAccount.sk).toString('hex');
+      const algorandPrivateKey = Buffer.from(algorandAccount.sk).toString(
+        "hex"
+      );
       const algorandMnemonic = algosdk.secretKeyToMnemonic(algorandAccount.sk);
 
       // Tezos Wallet
@@ -92,26 +105,64 @@ class WalletController {
       const stellarAddress = stellarWallet.getPublicKey(0);
       const stellarPrivateKey = stellarWallet.getSecret(0);
 
+      const encryptedMnemonic = encryptString(mnemonic);
+
       // Respond with addresses only
       res.json({
         success: true,
-        message: 'Wallet created successfully',
+        message: "Wallet created successfully",
         wallets: {
-          evm: { address: ethAddress, privateKey: ethPrivateKey, mnemonic: mnemonic },
-          brc: { address: btcAddress, privateKey: btcPrivateKey, mnemonic: mnemonic },
-          svm: { address: solAddress, privateKey: solPrivateKey, mnemonic: mnemonic },
-          tvm: { address: tonAddress, publicKey: tonPublicKey, privateKey: tonPrivateKey, mnemonic: mnemonicTon },
-          cosmos: { address: cosmosAddress, privateKey: cosmosPrivateKey, mnemonic: mnemonic },
-          substrate: { address: polkadotAddress, privateKey: polkadotPrivateKey, mnemonic: mnemonic },
-          algorand: { address: algorandAddress, privateKey: algorandPrivateKey, mnemonic: algorandMnemonic },
-          tezos: { address: tezosAddress, privateKey: tezosPrivateKey, mnemonic: mnemonic },
-          stellar: { address: stellarAddress, privateKey: stellarPrivateKey, mnemonic: mnemonic },
+          evm: {
+            address: ethAddress,
+            privateKey: encryptString(ethPrivateKey),
+            mnemonic: encryptedMnemonic,
+          },
+          brc: {
+            address: btcAddress,
+            privateKey: encryptString(btcPrivateKey),
+            mnemonic: encryptedMnemonic,
+          },
+          svm: {
+            address: solAddress,
+            privateKey: encryptString(solPrivateKey),
+            mnemonic: encryptedMnemonic,
+          },
+          tvm: {
+            address: tonAddress,
+            publicKey: tonPublicKey,
+            privateKey: encryptString(tonPrivateKey),
+            mnemonic: encryptString(mnemonicTon),
+          },
+          cosmos: {
+            address: cosmosAddress,
+            privateKey: encryptString(cosmosPrivateKey),
+            mnemonic: encryptedMnemonic,
+          },
+          substrate: {
+            address: polkadotAddress,
+            privateKey: encryptString(polkadotPrivateKey),
+            mnemonic: encryptedMnemonic,
+          },
+          algorand: {
+            address: algorandAddress,
+            privateKey: encryptString(algorandPrivateKey),
+            mnemonic: encryptString(algorandMnemonic),
+          },
+          tezos: {
+            address: tezosAddress,
+            privateKey: encryptString(tezosPrivateKey),
+            mnemonic: encryptedMnemonic,
+          },
+          stellar: {
+            address: stellarAddress,
+            privateKey: encryptString(stellarPrivateKey),
+            mnemonic: encryptedMnemonic,
+          },
         },
       });
-
     } catch (err) {
-      console.error('[Wallet Creation Error]', err.message);
-      res.status(500).json({ error: 'Wallet creation failed' });
+      console.error("[Wallet Creation Error]", err.message);
+      res.status(500).json({ error: "Wallet creation failed" });
     }
   }
 }
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,19 +1,19 @@
-const bip39 = require('bip39');
-const ecc = require('tiny-secp256k1');
-const bip32 = require('bip32');
-const bitcoin = require('bitcoinjs-lib');
-const solanaWeb3 = require('@solana/web3.js');
-const { TonClient, WalletContractV4, internal } = require('@ton/ton');
-const { mnemonicNew, mnemonicToPrivateKey } = require('@ton/crypto');
-const { Secp256k1HdWallet, Slip10RawIndex } = require('@cosmjs/amino');
-const { Keyring } = require('@polkadot/keyring');
-const { InMemorySigner } = require('@taquito/signer');
-const algosdk = require('algosdk');
-const StellarHDWallet = require('stellar-hd-wallet');
+const bip39 = require("bip39");
+const ecc = require("tiny-secp256k1");
+const bip32 = require("bip32");
+const bitcoin = require("bitcoinjs-lib");
+const solanaWeb3 = require("@solana/web3.js");
+const { TonClient, WalletContractV4, internal } = require("@ton/ton");
+const { mnemonicNew, mnemonicToPrivateKey } = require("@ton/crypto");
+const { Secp256k1HdWallet, Slip10RawIndex } = require("@cosmjs/amino");
+const { Keyring } = require("@polkadot/keyring");
+const { InMemorySigner } = require("@taquito/signer");
+const algosdk = require("algosdk");
+const StellarHDWallet = require("stellar-hd-wallet");
 
 // Using pg (node-postgres) library
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg");
+require("dotenv").config();
 
 const generateWallets = async (mnemonic) => {
   const seed = await bip39.mnemonicToSeed(mnemonic);
@@ -24,7 +24,10 @@ const generateWallets = async (mnemonic) => {
   const btcNode = root.derivePath("m/84'/0'/0'/0/0");
   wallets.bitcoin = {
     privateKey: btcNode.toWIF(),
-    address: bitcoin.payments.p2wpkh({ pubkey: Buffer.from(btcNode.publicKey), network: bitcoin.networks.bitcoin }).address,
+    address: bitcoin.payments.p2wpkh({
+      pubkey: Buffer.from(btcNode.publicKey),
+      network: bitcoin.networks.bitcoin,
+    }).address,
   };
 
   // Ethereum Wallet
@@ -38,41 +41,58 @@ const generateWallets = async (mnemonic) => {
   const solSeed = seed.slice(0, 32);
   const solKeypair = solanaWeb3.Keypair.fromSeed(solSeed);
   wallets.solana = {
-    privateKey: Buffer.from(solKeypair.secretKey).toString('hex'),
+    privateKey: Buffer.from(solKeypair.secretKey).toString("hex"),
     address: solKeypair.publicKey.toBase58(),
   };
 
   // TON Wallet
   const keyPair = await mnemonicToPrivateKey(await mnemonicNew());
-  const wallet = WalletContractV4.create({ workchain: 0, publicKey: keyPair.publicKey });
-  const client = new TonClient({ endpoint: 'https://toncenter.com/api/v2/jsonRPC' });
+  const wallet = WalletContractV4.create({
+    workchain: 0,
+    publicKey: keyPair.publicKey,
+  });
+  const client = new TonClient({
+    endpoint: "https://toncenter.com/api/v2/jsonRPC",
+  });
   wallets.ton = {
-    privateKey: keyPair.secretKey.toString('hex'),
-    publicKey: keyPair.publicKey.toString('hex'),
+    privateKey: keyPair.secretKey.toString("hex"),
+    publicKey: keyPair.publicKey.toString("hex"),
     address: (await client.open(wallet)).address.toString(),
   };
 
   // Cosmos Wallet
-  const cosmosWallet = await Secp256k1HdWallet.fromMnemonic(mnemonic, { prefix: 'cosmos', hdPaths: [[Slip10RawIndex.hardened(44), Slip10RawIndex.hardened(118), Slip10RawIndex.hardened(0), Slip10RawIndex.normal(0), Slip10RawIndex.normal(0)]] });
-  const [{ address: cosmosAddress, privkey: cosmosPrivateKeyRaw }] = await cosmosWallet.getAccountsWithPrivkeys();
+  const cosmosWallet = await Secp256k1HdWallet.fromMnemonic(mnemonic, {
+    prefix: "cosmos",
+    hdPaths: [
+      [
+        Slip10RawIndex.hardened(44),
+        Slip10RawIndex.hardened(118),
+        Slip10RawIndex.hardened(0),
+        Slip10RawIndex.normal(0),
+        Slip10RawIndex.normal(0),
+      ],
+    ],
+  });
+  const [{ address: cosmosAddress, privkey: cosmosPrivateKeyRaw }] =
+    await cosmosWallet.getAccountsWithPrivkeys();
   wallets.cosmos = {
-    privateKey: Buffer.from(cosmosPrivateKeyRaw).toString('hex'),
+    privateKey: Buffer.from(cosmosPrivateKeyRaw).toString("hex"),
     address: cosmosAddress,
   };
 
   // Polkadot Wallet
-  const keyring = new Keyring({ type: 'sr25519' });
+  const keyring = new Keyring({ type: "sr25519" });
   const polkadotSeed = mnemonicToMiniSecret(mnemonic);
   const { publicKey, secretKey } = sr25519PairFromSeed(polkadotSeed);
   wallets.polkadot = {
-    privateKey: Buffer.from(secretKey).toString('hex'),
+    privateKey: Buffer.from(secretKey).toString("hex"),
     address: keyring.encodeAddress(publicKey),
   };
 
   // Algorand Wallet
   const algorandAccount = algosdk.generateAccount();
   wallets.algorand = {
-    privateKey: Buffer.from(algorandAccount.sk).toString('hex'),
+    privateKey: Buffer.from(algorandAccount.sk).toString("hex"),
     address: algorandAccount.addr.toString(),
   };
 
@@ -108,45 +128,43 @@ const pool = new Pool({
 async function closePool() {
   try {
     await pool.end();
-    console.log('Database pool closed successfully');
+    console.log("Database pool closed successfully");
   } catch (error) {
-    console.error('Error closing database pool:', error);
+    console.error("Error closing database pool:", error);
     throw error;
   }
 }
 
 // Handle graceful shutdown on different signals
-process.on('SIGINT', async () => {
-  console.log('SIGINT received, closing database pool...');
+process.on("SIGINT", async () => {
+  console.log("SIGINT received, closing database pool...");
   await closePool();
   process.exit(0);
 });
 
-process.on('SIGTERM', async () => {
-  console.log('SIGTERM received, closing database pool...');
+process.on("SIGTERM", async () => {
+  console.log("SIGTERM received, closing database pool...");
   await closePool();
   process.exit(0);
 });
 
-process.on('exit', () => {
-  console.log('Process exiting...');
+process.on("exit", () => {
+  console.log("Process exiting...");
 });
 
 // Handle uncaught exceptions
-process.on('uncaughtException', async (error) => {
-  console.error('Uncaught Exception:', error);
+process.on("uncaughtException", async (error) => {
+  console.error("Uncaught Exception:", error);
   await closePool();
   process.exit(1);
 });
 
 // Handle unhandled promise rejections
-process.on('unhandledRejection', async (reason, promise) => {
-  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+process.on("unhandledRejection", async (reason, promise) => {
+  console.error("Unhandled Rejection at:", promise, "reason:", reason);
   await closePool();
   process.exit(1);
 });
-
-
 
 /**
  * Check if a wallet address or multiple addresses exist in the database
@@ -158,12 +176,12 @@ async function checkAddressExists(address) {
     // Handle array of addresses
     if (Array.isArray(address)) {
       if (address.length === 0) {
-        throw new Error('Address array cannot be empty');
+        throw new Error("Address array cannot be empty");
       }
 
       // Validate all addresses
-      if (!address.every(addr => addr && typeof addr === 'string')) {
-        throw new Error('All addresses must be valid strings');
+      if (!address.every((addr) => addr && typeof addr === "string")) {
+        throw new Error("All addresses must be valid strings");
       }
 
       const query = `
@@ -185,16 +203,16 @@ async function checkAddressExists(address) {
         exists: result.rows.length > 0,
         count: result.rows.length,
         wallets: result.rows,
-        foundAddresses: result.rows.map(row => row.address),
-        notFoundAddresses: address.filter(addr => 
-          !result.rows.some(row => row.address === addr)
-        )
+        foundAddresses: result.rows.map((row) => row.address),
+        notFoundAddresses: address.filter(
+          (addr) => !result.rows.some((row) => row.address === addr)
+        ),
       };
     }
 
     // Handle single address
-    if (!address || typeof address !== 'string') {
-      throw new Error('Invalid address provided');
+    if (!address || typeof address !== "string") {
+      throw new Error("Invalid address provided");
     }
 
     const query = `
@@ -216,17 +234,16 @@ async function checkAddressExists(address) {
     if (result.rows.length > 0) {
       return {
         exists: true,
-        wallet: result.rows[0]
+        wallet: result.rows[0],
       };
     }
 
     return {
       exists: false,
-      wallet: null
+      wallet: null,
     };
-
   } catch (error) {
-    console.error('Error checking address:', error);
+    console.error("Error checking address:", error);
     throw error;
   }
 }
@@ -241,11 +258,11 @@ async function addressExists(address) {
     // Handle array of addresses
     if (Array.isArray(address)) {
       if (address.length === 0) {
-        throw new Error('Address array cannot be empty');
+        throw new Error("Address array cannot be empty");
       }
 
-      if (!address.every(addr => addr && typeof addr === 'string')) {
-        throw new Error('All addresses must be valid strings');
+      if (!address.every((addr) => addr && typeof addr === "string")) {
+        throw new Error("All addresses must be valid strings");
       }
 
       const query = `
@@ -259,8 +276,8 @@ async function addressExists(address) {
     }
 
     // Handle single address
-    if (!address || typeof address !== 'string') {
-      throw new Error('Invalid address provided');
+    if (!address || typeof address !== "string") {
+      throw new Error("Invalid address provided");
     }
 
     const query = `
@@ -273,14 +290,11 @@ async function addressExists(address) {
 
     const result = await pool.query(query, [address]);
     return result.rows[0].exists;
-
   } catch (error) {
-    console.error('Error checking address:', error);
+    console.error("Error checking address:", error);
     throw error;
   }
 }
-
-
 
 /**
  * Get wallets by user_id or multiple user_ids
@@ -292,12 +306,12 @@ async function getWalletsByUserId(userId) {
     // Handle array of user_ids
     if (Array.isArray(userId)) {
       if (userId.length === 0) {
-        throw new Error('User ID array cannot be empty');
+        throw new Error("User ID array cannot be empty");
       }
 
       // Validate all user_ids
-      if (!userId.every(id => id && typeof id === 'string')) {
-        throw new Error('All user IDs must be valid strings');
+      if (!userId.every((id) => id && typeof id === "string")) {
+        throw new Error("All user IDs must be valid strings");
       }
 
       const query = `
@@ -323,8 +337,8 @@ async function getWalletsByUserId(userId) {
 
       // Group wallets by user_id
       const walletsByUser = {};
-      userId.forEach(id => {
-        walletsByUser[id] = result.rows.filter(row => row.user_id === id);
+      userId.forEach((id) => {
+        walletsByUser[id] = result.rows.filter((row) => row.user_id === id);
       });
 
       return {
@@ -332,16 +346,16 @@ async function getWalletsByUserId(userId) {
         count: result.rows.length,
         wallets: result.rows,
         walletsByUser: walletsByUser,
-        userIdsFound: [...new Set(result.rows.map(row => row.user_id))],
-        userIdsNotFound: userId.filter(id => 
-          !result.rows.some(row => row.user_id === id)
-        )
+        userIdsFound: [...new Set(result.rows.map((row) => row.user_id))],
+        userIdsNotFound: userId.filter(
+          (id) => !result.rows.some((row) => row.user_id === id)
+        ),
       };
     }
 
     // Handle single user_id
-    if (!userId || typeof userId !== 'string') {
-      throw new Error('Invalid user ID provided');
+    if (!userId || typeof userId !== "string") {
+      throw new Error("Invalid user ID provided");
     }
 
     const query = `
@@ -368,31 +382,152 @@ async function getWalletsByUserId(userId) {
     return {
       found: result.rows.length > 0,
       count: result.rows.length,
-      wallets: result.rows
+      wallets: result.rows,
     };
-
   } catch (error) {
-    console.error('Error getting wallets by user ID:', error);
+    console.error("Error getting wallets by user ID:", error);
     throw error;
   }
 }
 
+/**
+ * Encrypts a string using AES-GCM with a password-derived key
+ * @param {string} text - The text to encrypt
+ * @param {string} salt - The salt/password to use for encryption (optional, falls back to ENCRYPTION_SALT from .env)
+ * @returns {Promise<string>} Base64 encoded encrypted data with IV
+ */
+async function encryptString(text, salt) {
+  // Use provided salt or fall back to environment variable
+  const encryptionSalt = salt || process.env.ENCRYPTION_SALT;
 
-// Usage examples:
-async function examples() {
-  const testAddress = '0x1234567890abcdef';
+  if (!encryptionSalt) {
+    throw new Error(
+      "Salt is required. Provide a salt parameter or set ENCRYPTION_SALT in .env file"
+    );
+  }
 
-  // Example 1: Get full wallet info if exists
-  const result1 = await checkAddressExists(testAddress);
-  console.log(result1);
-  // Output: { exists: true/false, wallet: {...} or null }
+  const encoder = new TextEncoder();
 
-  // Example 2: Just check if exists (boolean)
-  const result2 = await addressExists(testAddress);
-  console.log(result2);
-  // Output: true or false
+  // Convert salt to key using PBKDF2
+  const saltBuffer = encoder.encode(encryptionSalt);
+  const keyMaterial = await crypto.subtle.importKey(
+    "raw",
+    saltBuffer,
+    "PBKDF2",
+    false,
+    ["deriveBits", "deriveKey"]
+  );
+
+  const key = await crypto.subtle.deriveKey(
+    {
+      name: "PBKDF2",
+      salt: saltBuffer,
+      iterations: 10000,
+      hash: "SHA-256",
+    },
+    keyMaterial,
+    { name: "AES-GCM", length: 256 },
+    false,
+    ["encrypt", "decrypt"]
+  );
+
+  // Generate random IV
+  const iv = crypto.getRandomValues(new Uint8Array(12));
+
+  // Encrypt the text
+  const encryptedBuffer = await crypto.subtle.encrypt(
+    { name: "AES-GCM", iv: iv },
+    key,
+    encoder.encode(text)
+  );
+
+  // Combine IV and encrypted data
+  const combined = new Uint8Array(iv.length + encryptedBuffer.byteLength);
+  combined.set(iv, 0);
+  combined.set(new Uint8Array(encryptedBuffer), iv.length);
+
+  // Convert to base64
+  return btoa(String.fromCharCode(...combined));
 }
 
+/**
+ * Decrypts a string encrypted with encryptString
+ * @param {string} encryptedText - Base64 encoded encrypted data
+ * @param {string} salt - The salt/password used for encryption (optional, falls back to ENCRYPTION_SALT from .env)
+ * @returns {Promise<string>} The decrypted text
+ */
+async function decryptString(encryptedText, salt) {
+  if (encryptedText.length == 0) return encryptedText;
+  // Use provided salt or fall back to environment variable
+  const encryptionSalt = salt || process.env.ENCRYPTION_SALT;
+
+  if (!encryptionSalt) {
+    throw new Error(
+      "Salt is required. Provide a salt parameter or set ENCRYPTION_SALT in .env file"
+    );
+  }
+
+  const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
+
+  // Convert base64 to buffer
+  const combined = Uint8Array.from(atob(encryptedText), (c) => c.charCodeAt(0));
+
+  // Extract IV and encrypted data
+  const iv = combined.slice(0, 12);
+  const encryptedData = combined.slice(12);
+
+  // Convert salt to key using PBKDF2
+  const saltBuffer = encoder.encode(encryptionSalt);
+  const keyMaterial = await crypto.subtle.importKey(
+    "raw",
+    saltBuffer,
+    "PBKDF2",
+    false,
+    ["deriveBits", "deriveKey"]
+  );
+
+  const key = await crypto.subtle.deriveKey(
+    {
+      name: "PBKDF2",
+      salt: saltBuffer,
+      iterations: 10000,
+      hash: "SHA-256",
+    },
+    keyMaterial,
+    { name: "AES-GCM", length: 256 },
+    false,
+    ["encrypt", "decrypt"]
+  );
+
+  // Decrypt the data
+  const decryptedBuffer = await crypto.subtle.decrypt(
+    { name: "AES-GCM", iv: iv },
+    key,
+    encryptedData
+  );
+
+  return decoder.decode(decryptedBuffer);
+}
+
+// Example usage:
+(async () => {
+  const originalText = "Hello, this is a secret message!";
+  const salt = "mySecretPassword123";
+
+  console.log("Original text:", originalText);
+
+  // Encrypt
+  const encrypted = await encryptString(originalText, salt);
+  console.log("Encrypted:", encrypted);
+
+  // Decrypt
+  const decrypted = await decryptString(encrypted, salt);
+  console.log("Decrypted:", decrypted);
+
+  // Verify
+  console.log("Match:", originalText === decrypted);
+})();
 
 // Export functions
 module.exports = {
@@ -400,4 +535,6 @@ module.exports = {
   checkAddressExists,
   getWalletsByUserId,
   addressExists,
+  encryptString,
+  decryptString,
 };
